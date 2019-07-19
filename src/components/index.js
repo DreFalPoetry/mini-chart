cff3ef063d8683736cb00d0e881b3cc24afba061
RanranLiu111 <864749024@qq.com>
2019-07-19 09:42:41 +0800

    修改为只传递数据到组件

--- src/components/index.js
+++ src/components/index.js
@@ -3,37 +3,12 @@ import {
   Chart,
   Geom,
 } from "bizcharts";
-import {fetchingData} from '../utils'; 
 import './styles.css';
 
 class MiniArea extends React.Component {
   constructor(props){
     super(props)
-    this.fetchData = this.fetchData.bind(this)
     this.state = {}
-  }
-
-  componentDidMount(){
-    const {url,setChartData,uniqueKey} = this.props;
-    this.fetchData(url,setChartData,uniqueKey)
-  }
-
-  componentWillReceiveProps(nextProps){
-    if(nextProps.uniqueKey!== this.props.uniqueKey){
-      const {url,setChartData,uniqueKey} = nextProps;
-      this.fetchData(url,setChartData,uniqueKey)
-    }
-  }
-
-  fetchData(url,setChartData,uniqueKey){
-    console.log(uniqueKey);
-    if(!window.isFetching){
-      if(!setChartData || !url) return;
-      const response = fetchingData(url)
-      response.then((data)=>{
-        this.props.setChartData(data,uniqueKey)
-      })
-    }
   }
   
 	render() {
